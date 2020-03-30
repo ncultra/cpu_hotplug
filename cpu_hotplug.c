13eeeea850deb0d34e44f80e5ac127761e5c36a6
Mike Day <ncultra@gmail.com>
2020-03-30 18:41:05 -0400

    callbacks working

--- cpu_hotplug.c
+++ cpu_hotplug.c
@@ -1,16 +1,37 @@
 #include "cpu_hotplug.h"
 
+static int my_cpu_online(unsigned int cpu)
+{
+	int ccode = 0;
+	printk(KERN_DEBUG "cpu %d coming online\n", cpu);
+	return ccode;
+}
+
+
+static int my_cpu_going_offline(unsigned int cpu)
+{
+	int ccode = 0;
+	printk(KERN_DEBUG "cpu %d going offline\n", cpu);
+	return ccode;
+}
 
 static int __init cpu_hotplug_init(void)
 {
-  int ccode = 0;
-  printk(KERN_DEBUG "cpu hotplug demo module\n");
-  return ccode;
+	int ccode = 0;
+	printk(KERN_DEBUG "cpu hotplug demo module\n");
+	ccode = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN,
+				  "x86/demo:online",
+				  my_cpu_online,
+				  my_cpu_going_offline);
+
+	printk(KERN_DEBUG "cpuhp_setup_state returned %d\n", ccode);
+	return 0;
 }
 
 static void __exit cpu_hotplug_cleanup(void)
 {
-  printk(KERN_DEBUG "cpu hotplug demo unloading...\n");
+	printk(KERN_DEBUG "cpu hotplug demo unloading...\n");
+	cpuhp_remove_state(CPUHP_AP_ONLINE_DYN);
 }
 
 
